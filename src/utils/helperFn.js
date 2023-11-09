3cd28c997a29ed97e9154c98824d5e553eddd370
dingneng.pan <dingneng.pan@yunqutech.com>
2023-11-09 14:23:58 +0800

    fix: 完善utils

--- src/utils/helperFn.js
+++ src/utils/helperFn.js
@@ -1,52 +1,96 @@
+Function.prototype.myCall = function(context) {
+  if (typeof this !== 'function') {
+    throw new TypeError('this is not a function')
+  }
+  context = context || wondow
+  const args = Array.from(arguments).slice(1)
+  const fn = Symbol()
+  context[fn] = this
+  let result = context[fn](...args)
+  delete context[fn]
+  return result
+};
+Function.prototype.myApply = function(context) {
+  if (typeof this !== 'function') {
+    throw new TypeError('this is not a function')
+  }
+  context = context || window
+  let result;
+  const fn = Symbol()
+  context[fn] = this
+  if (arguments[1]) {
+    result = context[fn](...args)
+  } else {
+    result = context[fn]()
+  }
+  delete context[fn]
+  return result
+};
+Function.prototype.myBind = function(context) {
+  if (typeof this !== 'function') {
+    throw new TypeError('this is not a funcion')
+  }
+  const _this = this
+  let args = Array.prototype.slice.call(arguments, 1)
+  return function F() {
+    if (this instanceof F) {
+      return new _this(...args, ...arguments)
+    } else {
+      return _this.apply(context, args.context(...arguments))
+    }
+  }
+};
 function isObject(value) {
-  const type = typeof value;
-  return value !== null && (type === 'object' || type == 'function')
+  const type = typeof value
+  return value !== null && (type === 'object' || type === 'function')
 }
 function toType(value) {
-  return Object.prototype.toString.call(value).slice(8, -1)
+  return Object.prototype.toString.call(value).slice(8)
 }
-export const cloneDeep = function (value, stack = new Map()) {
+function cloneDeep(value, stack = new Map()) {
   if (!isObject(value)) return value;
-  let type = toType(value);
-  let ctor = value.constructor;
-  if (type === 'RegExp' || type === 'date') return new ctor(value);
+  const type = toType(value);
+  const ctor = value.constructor
 
+  if (type === 'RegExp' || type === 'Date') return new ctor()
   if (type === 'Error') return new ctor(value.message)
-
-  if (type === 'Function') return function() {
+  if (type === 'function') return function () {
     value.apply(this, arguments)
   }
 
   let result = new ctor()
-  const stacked = stack.get(value)
+  let stacked = stack.get(value)
   if (stacked) {
     return stacked
   }
   stack.set(value, result)
-
-  if (type === 'Map') {
-    value.forEach((item, index) => {
-      result.set(index, cloneDeep(item, stack))
-    })
-    return result;
-  }
-
   if (type === 'Set') {
-    value.forEach((item) => {
-      result.add(cloneDeep(item, stack))
+    value.forEach(i => {
+      result.add(cloneDeep(i, stack))
     })
+    return result
   }
-
+  if (type === 'Map') {
+    value.forEach((val, key) => {
+      result.set(cloneDeep(key, stack), cloneDeep(val, stack))
+    })
+    return result
+  }
   Object.keys(value).forEach(key => {
     result[key] = cloneDeep(value[key], stack)
   })
-  return result;
+  return result
 }
-
-const PENDING = 'pending';
-const FULFILLED ='fulfilled';
-const REJECTED = 'rejected';
-class MyPromise {
+function myNew (fn, ...args) {
+  let obj = {}
+  obj.__proto__ = fn.prototype
+  let result = fn.apply(obj, args)
+  return result instanceof Object ? result : obj
+}
+const PENDING = 'pending'
+const FULFILLED = 'fulfilled'
+const REJECTED = 'rejected'
+class Mypromise {
   constructor(executor) {
     try {
       executor(this.resolve, this.reject)
@@ -55,242 +99,145 @@ class MyPromise {
     }
   }
 
-  status = PENDING;
-  value = null;
-  reason = null;
-
-  onFulfilledCallbacks = [];
-  onRejectedCallbacks = [];
+  status = PENDING
+  value = null
+  reason = null
+  onFulfilledCallbacks = []
+  onRejectedCallbacks = []
 
   resolve = (value) => {
     if (this.status === PENDING) {
-      this.status = FULFILLED;
-      this.value = value;
-
-      while(this.onFulfilledCallbacks.length) {
-        this.onFulfilledCallbacks.shift()(value)
+      this.status = FULFILLED
+      this.value = value
+      if (this.onFulfilledCallbacks.length) {
+        this.onFulfilledCallbacks.shift()(this.value)
       }
     }
   }
-
   reject = (reason) => {
-    if (this.status === PENDING) {
-      this.status = REJECTED;
-      this.reason = reason;
-
-      while(this.onRejectedCallbacks.length) {
-        this.onRejectedCallbacks.shift()(value)
+    if (this.status = PENDING) {
+      this.status = REJECTED
+      this.reason = reason
+      if (this.onRejectedCallbacks.length) {
+        this.onRejectedCallbacks.shift()(this.reason)
       }
     }
   }
-
+  static resolve(parameter) {
+    if (parameter instanceof Mypromise) {
+      return parameter
+    }
+    return new Mypromise((rs, rj) => {
+      rs(parameter)
+    })
+  }
+  static rejected(parameter) {
+    return new Mypromise((rs, rj) => {
+      rj(parameter)
+    })
+  }
   then(onFulfilled, onRejected) {
-    const realOnFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value;
-    const realOnRejected = typeof onRejected === 'functon' ? onRejected : reason => reason;
-
-    const promise2 = new MyPromise((resolve, reject) => {
+    const realFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value
+    const realRejected = typeof onRejected === 'function' ? onRejected : reason => reason
+    const promise2 = new Promise((rs, rj) => {
       const fulfilledMicrotask = () => {
         queueMicrotask(() => {
           try {
-            const x = realOnFulfilled(this.value)
-            resolvePromise(promise2, x, resolve, reject)
+            const x = realFulfilled(this.value);
+            resolvePromise(promise2, x, rs, rj)
           } catch (error) {
-            reject(error)
+            this.reject(error)
           }
         })
       }
-
       const rejectedMicrotask = () => {
         queueMicrotask(() => {
           try {
-            const x = realOnRejected(this.reason)
-            resolvePromise(promise2, x, resolve, reject)
+            const x = realRejected(this.reason)
+            resolvePromise(promise2, x, rs, rj)
           } catch (error) {
-            reject(error)
+            this.reject(error)
           }
         })
       }
-
       if (this.status === FULFILLED) {
         fulfilledMicrotask()
       } else if (this.status === REJECTED) {
         rejectedMicrotask()
-      } else if (this.status === PENDING) {
+      } else {
         this.onFulfilledCallbacks.push(fulfilledMicrotask)
         this.onRejectedCallbacks.push(rejectedMicrotask)
       }
     })
-    return promise2;
-  }
-
-  static resolve(parameter) {
-    if (parameter instanceof MyPromise) {
-      return parameter;
-    }
-
-    return new MyPromise((resolve) => {
-      resolve(parameter)
-    })
-  }
-
-  static reject(parameter) {
-    return new MyPromise((resolve, reject) => {
-      reject(parameter)
-    })
+    return promise2
   }
 }
-
-function resolvePromise(promise2, x, resolve, reject) {
+function resolvePromise(promise2, x, rs, rj) {
   if (promise2 === x) {
-    return reject(new TypeError('chaining cycle detect from promise'))
+    return new TypeError('chaining cycle detect from Mypromise')
   }
-
-  if (x instanceof MyPromise) {
-    x.then(resolve, reject)
+  if (x instanceof Mypromise) {
+    x.then(rs, rj)
   } else {
-    resolve(x)
+    rs(x)
   }
 }
-
-MyPromise.all = (promises) => {
-  return new MyPromise((rs, rj) => {
-    let count = 0;
-    let result = [];
-
-    const len = promises.length;
-    if (len === 0) {
+Mypromise.all = (promises) => {
+  return new Mypromise((rs, rj) => {
+    const len = promises.length
+    let count = 0
+    let result = []
+    if (!len) {
       return rs([])
     }
-
     promises.forEach((p, i) => {
-      MyPromise.resolve(p).then((res) => {
-        count += 1;
-        result[i] = res;
 
-        if (len === count) {
-          rs(result)
-        }
-      }).catch(rj);
     })
   })
 }
+function myTypeof(value, fullClass) {
+  if (fullClass) return value === null ? '[object Null]' : Object.prototype.toString.call(value)
+  if (value == null) return (value + '').toLowerCase()
+  const type = Object.prototype.toString.call(value).slice(8).toLowerCase()
+  if (type === 'generatorfunction') return 'function'
 
-function add() {
-  var args = Array.prototype.slice.call(arguments)
-
-  var adder = function () {
-    args.push(...arguments)
-    return adder
-  }
-
-  adder.toString = function () {
-    return args.reduce((prev, curr) => {return prev + curr}, 0)
-  }
-
-  return adder
+  return type.match(/^(array|bigint|error|function|generator|regexp|date|symbol)$/) ? type :
+    (typeof value === 'function' || typeof value === 'object') ? 'object' : typeof value
 }
-add(1)(2,3,4)(5).toString()
+function myInstanceOf(instance, object) {
+  if (object[Symbol.hasInstance]) return object[Symbol.hasInstance](instance)
+  else {
+    if (instance === null || typeof instance !== 'object') return false
+    if (object === Array) return Array.isArray(instance)
 
-
-
-
-class PubSub {
-  constructor() {
-    this.subscribers = {};
-  }
-
-  subscribe(event, callback) {
-    if (!this.subscribers[event]) {
-      this.subscribers[event] = [];
+    let proto = Object.getPrototypeOf(instance)
+    while(proto !== null) {
+      if (proto === object.prototype) return true
+      proto = Object.getPrototypeOf(proto)
     }
-    this.subscribers[event].push(callback)
-  }
-
-  publish(event, data) {
-    if (!this.subscribers[event]) return;
-    this.subscribers[event].forEach(callback => callback(data))
+    return false
   }
 }
-
-let data = {
-  text: 'Hello World',
-  pubsub: new PubSub()
-}
-
-Object.defineProperty(data, 'text', {
-  get: function() {
-    return this._text
-  },
-  set: function (value) {
-    this._text = value;
-    this.pubsub.publish('textChanged', value)
-  }
-})
-data.pubsub.subscribe('textChanged', function(value) {
-  console.log(value)
-})
-
-class Scheduler {
-  constructor(maxCouncurrent) {
-    this.taskList = []
-    this.count = 0;
-    this.maxCount = maxCouncurrent
-  }
-
-  async add (fn) {
-    if (this.count >= this.maxCount) {
-      return new Promise((resolve) => {
-        this.taskList.push(() => {
-          this.count ++;
-          fn().then(res => {
-            this.count --
-            resolve(res)
-            if (this.taskList.length > 0) {
-              this.taskList.shift()()
-            }
-          })
-        })
-      })
-    } else {
-      this.count ++
-      return fn().then(res => {
-        this.count --
-        if (this.taskList.length > 0) {
-          this.taskList.shift()()
-        }
-        return res
-      })
-    }
-  }
-}
-const scheduler = new Scheduler(2)
-const addTask = (time, order) => {
-  scheduler.add(()=>new Promise(r=>setTimeout(() => {r(order)},time)))
-      .then((res)=>console.log(res, order))
-}
-//log:2 1 3 4 4
-
-/* 数组去重 */
 function uniqueArray(arr) {
-  const result = []
+  let result = []
   for (let i = 0; i < arr.length; i++) {
     const item1 = arr[i]
     let isFind = false
     for (let j = 0; j < result.length; j++) {
       const item2 = result[j]
-      if (equals(item1, item2)) {
+      if (isEqual(item1, item2)) {
         isFind = true
         break
       }
     }
-    if (!isFind) {
+    if (isFind) {
       result.push(item1)
     }
   }
   return result
 }
-function equals(value1, value2) {
-  if (isPrimitive(value1) || isPrimitive(value2)) {
+function isEqual(value1, value2) {
+  if (!isObject(value1) || !isObject(value2)) {
     return Object.is(value1, value2)
   }
   const entries1 = Object.entries(value1)
@@ -298,16 +245,177 @@ function equals(value1, value2) {
   if (entries1.length !== entries2.length) {
     return false
   }
-  for (const [key, value] of entries1) {
-    if (!equals(value, value2[key])) {
+  for (const [key, val] of entries1) {
+    if (!isEqual(val, entries2[key])) {
       return false
     }
   }
   return true
 }
-function isPrimitive(value) {
-  return value === null || !['object', 'function'].includes(typeof value)
+function flat(arr, n = 1) {
+  function deep(item, i) {
+    if (i === n) {
+      return item
+    } else {
+      if (Array.isArray(item)) {
+        return item.reduce((prev, curr) => prev.concat(deep(curr, i+1)), [])
+      } else return item
+    }
+  }
+  return deep(arr, 0)
 }
-
-
-
+function getFps(wait) {
+  const prev = + new Date()
+  function loop() {
+    const now = + new Date()
+    if (now - prev >= wait) {
+      console.log(fps)
+    } else {
+      fps++
+      requestAnimationFrame(loop)
+    }
+  }
+  requestAnimationFrame(loop)
+}
+function checkStuck() {
+  let prev = + new Date()
+  let count = 0
+  let isStuck = false
+  function loop() {
+    const now = + new Date()
+    if (now - prev > 1000/60*1.5) {
+      count++
+      if (count >= 3 && !isStuck) {
+        console.log('系统卡顿')
+        setTimeout(() => {
+          count = 0
+          isStuck = false
+          console.log('系统重启')
+        }, 3000)
+      }
+    } else {
+      count = 0
+    }
+    prev = now
+    requestAnimationFrame(loop)
+  }
+  requestAnimationFrame(loop)
+}
+class EventBus {
+  constructor () {
+    this.map = {}
+  }
+  on(event, fn) {
+    const { map } = this
+    if (map[event]) map[event].push(fn)
+    else map[event] = fn
+    return this
+  }
+  emit(event, ...args) {
+    const fns = this.map[event] || []
+    (async () => {
+      for (let i = 0; i < fns.length; i++) {
+        fns[i].apply(this, args)
+      }
+    })()
+    return this
+  }
+  once(event, fn) {
+    const { map } = this
+    const F = (...args) => {
+      fn.apply(this, args)
+      const index = map[event].indexOf(F)
+      map[event].splice(index, 1)
+    }
+    if (map[event]) map[event].push(F)
+    else map[event] = [F]
+    return this
+  }
+  off(event, fn) {
+    const { map } = this;
+    if (map[event] === undefined) return false
+    const index = map[event].indexOf(fn)
+    if (index >= 0) {
+      map[event].splice(index, 1)
+      return true
+    }
+    return false
+  }
+}
+function promiseQueue(promises, maxCount) {
+  return new Promise((rs, rj) => {
+    let index = 0
+    let complete = 0
+    let total = promises.length
+    let result = []
+    function next() {
+      if (total === complete) {
+        rs(result)
+      }
+      while(index < total && maxCount > 0) {
+        maxCount--
+        let current = index++
+        promises[current].then((res) => {
+          maxCount++
+          complete++
+          result[current] = res
+          next()
+        }).catch((err) => {
+          rs(err)
+        });
+      }
+    }
+    next()
+  })
+}
+function testPromiseQueue() {
+  const promises = [
+    new Promise((rs, rj) => setTimeout(() => rs(1), 1000)),
+    new Promise((rs, rj) => setTimeout(() => rs(2), 2000)),
+    new Promise((rs, rj) => setTimeout(() => rs(3), 3000)),
+  ]
+  promiseQueue(promises, 2)
+}
+function add() {
+  let args = Array.prototype.slice.call(arguments)
+  function addr() {
+    args.push(...arguments)
+    return addr
+  }
+  addr.toString = function() {
+    return args.reduce((prev, curr) => prev + curr, 0)
+  }
+}
+function debounce(fn, wait) {
+  let timer
+  return function(...args) {
+    if (timer) clearTimeout()
+    timer = setTimeout(() => {
+      fn.apply(this, args)
+    }, wait)
+  }
+}
+function debouncedImmediate(fn, wait) {
+  let timer
+  return function(args) {
+    if (timer) clearTimeout(timer)
+    let callNow = !timer
+    timer = setTimeout(() => {
+      timer = null
+    }, wait)
+    if (callNow) { fn.apply(this, args) }
+  }
+}
+function throttleImmediate(fn, wait) {
+  let timer, result;
+  let throttled = function (...args) {
+    if (!timer) {
+      result = fn.apply(this, args)
+      timer = setTimeout(() => {
+        timer = null
+      }, wait)
+      return result
+    }
+  }
+  return throttled
+}
